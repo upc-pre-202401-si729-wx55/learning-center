6956e927ee446de63d5c5405253eb4a412cd8c6c
Angel Velasquez <pcisavel@upc.edu.pe>
2024-04-27 07:11:20 -0500

    feat(students): added in-app navigation route for student management component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./public/pages/home/home.component";
 import {AboutComponent} from "./public/pages/about/about.component";
 import {PageNotFoundComponent} from "./public/pages/page-not-found/page-not-found.component";
+import {StudentManagementComponent} from "./learning/pages/student-management/student-management.component";
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'learning/students', component: StudentManagementComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: '**', component: PageNotFoundComponent }
 ];
