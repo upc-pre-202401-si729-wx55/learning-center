2a817e495c161afb54753cf1808fe21361442bb5
Angel Velasquez <pcisavel@upc.edu.pe>
2024-06-10 22:07:48 -0500

    feat(iam): added sign up and sign in routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,16 @@ import {HomeComponent} from "./public/pages/home/home.component";
 import {AboutComponent} from "./public/pages/about/about.component";
 import {PageNotFoundComponent} from "./public/pages/page-not-found/page-not-found.component";
 import {StudentManagementComponent} from "./learning/pages/student-management/student-management.component";
+import {SignInComponent} from "./iam/pages/sign-in/sign-in.component";
+import {SignUpComponent} from "./iam/pages/sign-up/sign-up.component";
+import {authenticationGuard} from "./iam/services/authentication.guard";
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'learning/students', component: StudentManagementComponent },
+  { path: 'learning/students', component: StudentManagementComponent, canActivate: [authenticationGuard] },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: '**', component: PageNotFoundComponent }
 ];
